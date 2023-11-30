86d78cf0507f4e242898d019eaee8208e3c9bce5
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-01 00:01:46 +0530

    updated project configs

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sculptgen/utils/constants/text_strings.dart';
+import 'package:sculptgen/utils/themes/theme.dart';
+import 'package:get/get.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: FTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: FAppTheme.lightTheme,
+      darkTheme: FAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // home: const OnboardingScreen(),
+    );
+  }
+}
