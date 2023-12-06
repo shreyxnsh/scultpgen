950da5d41ec7a0c665881bdf08eeebffdad363ae
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-06 23:20:15 +0530

    SignupScreen completed

---
+++ lib/features/authentication/screens/signup.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:sculptgen/features/authentication/screens/login.dart';
+import 'package:sculptgen/utils/constants/colors.dart';
+import 'package:sculptgen/utils/constants/image_strings.dart';
+import 'package:sculptgen/utils/constants/sizes.dart';
+import 'package:sculptgen/utils/constants/text_strings.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: FSizes.xl,
+                  ),
+                  Image.asset(
+                    "assets/icons/sclogo.png",
+                    width: 120,
+                    height: 120,
+                  ),
+                  
+                  const SizedBox(
+                    height: FSizes.spaceBtwItems,
+                  ),
+                  Text(
+                    FTexts.signupTitle,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(
+                    height: FSizes.sm,
+                  ),
+                  Text(
+                    FTexts.signupSubTitle,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: FSizes.spaceBtwSections,
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.direct_right),
+                        labelText: FTexts.username),
+                  ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwItems,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.personalcard),
+                        labelText: FTexts.email),
+                  ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwItems,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.password_check),
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                        labelText: "Password"),
+                  ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwItems ,
+                  ),
+                  Row(
+          children: [
+            SizedBox(
+              width: 24,
+              height: 24,
+              child: Checkbox(
+                value: true,
+                onChanged: (value) {},
+              ),
+            ),
+            const SizedBox(
+              width: FSizes.spaceBtwItems / 2,
+            ),
+            Expanded(
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                        text: '${FTexts.iAgreeTo} ',
+                        style: Theme.of(context).textTheme.bodySmall),
+                    TextSpan(
+                        text: FTexts.privacyPolicy,
+                        style: Theme.of(context).textTheme.bodySmall!.apply(
+                              color:FColors.primary,
+                              decoration: TextDecoration.underline,
+                              decorationColor:
+                                   FColors.primary,
+                            )),
+                    TextSpan(
+                        text: ' ${FTexts.and} ',
+                        style: Theme.of(context).textTheme.bodySmall),
+                    TextSpan(
+                        text: FTexts.termsOfUse,
+                        style: Theme.of(context).textTheme.bodySmall!.apply(
+                              color: FColors.primary,
+                              decoration: TextDecoration.underline,
+                              decorationColor:
+                                  FColors.primary,
+                            )),
+                  ],
+                ),
+                overflow: TextOverflow.ellipsis, // Add this line
+              ),
+            )
+          ],
+        ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwSections,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(FTexts.createAccount),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwItems,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Get.to(
+                          LoginScreen()
+                        );
+                      },
+                      child: const Text(FTexts.alreadylogin),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: FSizes.spaceBtwSections,
+                  ),
+                ],
+              )),
+              const SizedBox(
+                height: FSizes.spaceBtwItems / 2,
+              ),
+              const SizedBox(
+          height: FSizes.spaceBtwItems,
+        ),
+        
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(child: Divider()),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    FTexts.orSignInWith.capitalize!,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: Divider()),
+                ],
+              ),
+              const SizedBox(
+                height: FSizes.spaceBtwItems ,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: FColors.grey), borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: FSizes.xl,
+                        height: FSizes.xl,
+                        image : AssetImage(FImages.google)
+                      ),
+                    )
+                  ),
+
+                  const SizedBox(width: FSizes.spaceBtwItems,),
+                  
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: FColors.grey), borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: FSizes.xl,
+                        height: FSizes.xl,
+                        image : AssetImage(FImages.facebook)
+                      ),
+                    )
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
