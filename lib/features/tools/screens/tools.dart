ef072ab0ee58aee4f0929a7c05bf1dfa68b623de
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-09 23:01:30 +0530

    created screens for bottomnav

---
+++ lib/features/tools/screens/tools.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import 'package:sculptgen/common/widgets/appbar/appbar.dart';
+import 'package:sculptgen/common/widgets/appbar/home_appbar.dart';
+import 'package:sculptgen/features/authentication/screens/login.dart';
+import 'package:sculptgen/providers/user_provider.dart';
+import 'package:sculptgen/utils/constants/sizes.dart';
+
+class ToolScreen extends StatelessWidget {
+  const ToolScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: FAppBar(title: Text("Tools"),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+        
+            Padding(
+              padding: EdgeInsets.all(FSizes.defaultSpace),
+              child: Column(
+                children: [
+                  
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
